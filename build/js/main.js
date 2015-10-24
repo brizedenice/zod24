2edafec65c328ce1852516816c1b172df3026abf
Илья Монастырёв <brizedenice@gmail.com>
2015-10-24 15:33:21 +0300

    Version 24.10.2015 15:33

--- build/js/main.js
+++ build/js/main.js
@@ -2,3 +2,26 @@ $('header nav a').click(function () {
 	$('header nav a').removeClass('active');
 	$(this).addClass('active');
 });
+
+$(window).load(function(){
+
+	$('textarea').autosize();
+
+	$(".dropdown ul").mCustomScrollbar({
+	    axis:"y" 
+	});
+
+});
+
+$(document).on('click', '.mobile-header .btn_menu', function(){
+	$(this).toggleClass('active');
+	$(this).parents('.mobile-header').find('.dropdown').toggleClass('active');
+	$('.mask').toggleClass('active');
+	$('body').toggleClass('active');
+});
+
+$(document).on('click', '.mobile-header .categoeies-list li.current', function(){
+	$(this).toggleClass('active');
+	$(this).parents('.categoeies-list').toggleClass('active');
+});
+
